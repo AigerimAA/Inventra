4a165eb39b1cd6a6b23fe0d970eecf8040bc99ae
AigerimAA <aigerim.10803@gmail.com>
2026-05-06 17:07:59 +0500

    fix: make FullTextSearch migration idempotent with existence checks

--- Inventra.Infrastructure/Migrations/20260503090138_AddFullTextSearch.cs
+++ Inventra.Infrastructure/Migrations/20260503090138_AddFullTextSearch.cs
@@ -10,28 +10,52 @@ namespace Inventra.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("CREATE FULLTEXT CATALOG InventraFtsCatalog AS DEFAULT;", suppressTransaction: true);
-
             migrationBuilder.Sql(@"
-                CREATE FULLTEXT INDEX ON Inventories(Title, Description) 
-                KEY INDEX PK_Inventories 
-                ON InventraFtsCatalog 
-                WITH (CHANGE_TRACKING = AUTO);", suppressTransaction: true);
+            IF FULLTEXTSERVICEPROPERTY('IsFullTextInstalled') = 1
+            BEGIN
+                IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'InventraFtsCatalog')
+                BEGIN
+                    CREATE FULLTEXT CATALOG InventraFtsCatalog AS DEFAULT;
+                END
 
-            migrationBuilder.Sql(@"
-                CREATE FULLTEXT INDEX ON Items(CustomId, CustomString1Value, CustomString2Value, 
-                    CustomString3Value, CustomText1Value, CustomText2Value, CustomText3Value) 
-                KEY INDEX PK_Items 
-                ON InventraFtsCatalog 
-                WITH (CHANGE_TRACKING = AUTO);", suppressTransaction: true);
-                }
+                IF NOT EXISTS (SELECT * FROM sys.fulltext_indexes WHERE object_id = OBJECT_ID('Inventories'))
+                BEGIN
+                    CREATE FULLTEXT INDEX ON Inventories(Title, Description) 
+                    KEY INDEX PK_Inventories 
+                    ON InventraFtsCatalog 
+                    WITH (CHANGE_TRACKING = AUTO);
+                END
+
+                IF NOT EXISTS (SELECT * FROM sys.fulltext_indexes WHERE object_id = OBJECT_ID('Items'))
+                BEGIN
+                    CREATE FULLTEXT INDEX ON Items(
+                        CustomId, CustomString1Value, CustomString2Value, 
+                        CustomString3Value, CustomText1Value, CustomText2Value, CustomText3Value
+                    ) 
+                    KEY INDEX PK_Items 
+                    ON InventraFtsCatalog 
+                    WITH (CHANGE_TRACKING = AUTO);
+                END
+            END
+            ", suppressTransaction: true);
+                    }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Items;", suppressTransaction: true);
-            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Inventories;", suppressTransaction: true);
-            migrationBuilder.Sql("DROP FULLTEXT CATALOG InventraFtsCatalog;", suppressTransaction: true);
+            migrationBuilder.Sql(@"
+                IF FULLTEXTSERVICEPROPERTY('IsFullTextInstalled') = 1
+                BEGIN
+                    IF EXISTS (SELECT * FROM sys.fulltext_indexes WHERE object_id = OBJECT_ID('Items'))
+                        DROP FULLTEXT INDEX ON Items;
+
+                    IF EXISTS (SELECT * FROM sys.fulltext_indexes WHERE object_id = OBJECT_ID('Inventories'))
+                        DROP FULLTEXT INDEX ON Inventories;
+
+                    IF EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'InventraFtsCatalog')
+                        DROP FULLTEXT CATALOG InventraFtsCatalog;
+                END
+                ", suppressTransaction: true);
         }
     }
 }
