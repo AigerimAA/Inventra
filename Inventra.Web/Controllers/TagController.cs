ecc8259206cedf97e7e617da2e6792b4ad439c80
AigerimAA <aigerim.10803@gmail.com>
2026-05-10 21:33:37 +0500

    feat: add tag autocomplete with TagController and tag UI in Edit inventory

---
+++ Inventra.Web/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using Inventra.Application.Interfaces;
+using Inventra.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventra.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                return Json(new List<string>());
+
+            var tags = await _tagRepository.GetByPrefixAsync(query);
+            return Json(tags.Select(t => t.Name).ToList());
+        }
+    }
+}
