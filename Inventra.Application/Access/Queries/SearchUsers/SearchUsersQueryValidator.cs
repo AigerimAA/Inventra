e5de71cf78dadb1aebf9e33b4abc452b3bce89d7
AigerimAA <aigerim.10803@gmail.com>
2026-05-12 00:26:12 +0500

    feat: add FluentValidation validator for SearchUsersQuery

---
+++ Inventra.Application/Access/Queries/SearchUsers/SearchUsersQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Inventra.Application.Access.Queries.SearchUsers
+{
+    public class SearchUsersQueryValidator : AbstractValidator<SearchUsersQuery>
+    {
+        public SearchUsersQueryValidator()
+        {
+            RuleFor(x => x.Query)
+                .NotEmpty().WithMessage("Search query is required")
+                .MinimumLength(2).WithMessage("Search query must be at least 2 characters");
+        }
+    }
+}
