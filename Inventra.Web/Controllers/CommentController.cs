e924de549b4063888f54e48852f9636bbee17cf1
AigerimAA <aigerim.10803@gmail.com>
2026-05-12 13:31:17 +0500

    fix: broadcast new comments via SignalR after saving

--- Inventra.Web/Controllers/CommentController.cs
+++ Inventra.Web/Controllers/CommentController.cs
@@ -1,18 +1,22 @@
 ﻿using Inventra.Application.Comments.Commands;
 using Inventra.Application.Comments.Queries;
+using Inventra.Web.Hubs;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Inventra.Web.Controllers
 {
     public class CommentController : Controller
     {
         private readonly IMediator _mediator;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        public CommentController(IMediator mediator)
+        public CommentController(IMediator mediator, IHubContext<ChatHub> hubContext)
         {
             _mediator = mediator;
+            _hubContext = hubContext;
         }
 
         [Authorize]
@@ -24,6 +28,12 @@ namespace Inventra.Web.Controllers
                 return RedirectToAction("Details", "Inventory", new { id = inventoryId });
 
             await _mediator.Send(new AddCommentCommand(inventoryId, content));
+
+            var userName = User.Identity?.Name ?? "Anonymous";
+            var timestamp = DateTime.UtcNow.ToString("dd.MM.yyyy HH:mm");
+            await _hubContext.Clients.Group(inventoryId.ToString())
+                .SendAsync("ReceiveMessage", userName, content, timestamp);
+
             return RedirectToAction("Details", "Inventory", new { id = inventoryId });
         }
 
